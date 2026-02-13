fded1a5b310e4b4cb265839f6fe9dd84f9a4d07d
a-r-t <alexthimineur@gmail.com>
2026-02-12 19:05:32 -0500

    added more exclamation marks and a hello message to my amazing volunteer Khang

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!!");
-        System.out.println("Welcome to GitHub ClASS!!");
+        System.out.println("Welcome to GitHub ClASS!!!!!!!!!!!!");
+        System.out.println("Hello Khang!!");
     }
 }
