7e01dcf91530cfd0d51d5243fe27c9d424dd803a
a-r-t <alexthimineur@gmail.com>
2026-02-26 19:02:54 -0500

    all caps!

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        System.out.println("HELLO WORLD!!");
         System.out.println("Welcome to GitHub ClASS!!!!!!!!!!!!");
         System.out.println("Hello Khang!!");
         System.out.println("Hello Mario!!");
